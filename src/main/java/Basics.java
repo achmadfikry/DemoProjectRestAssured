e6fff13acfe7b16469ea96e5f590177d02028d57
Achmad Fikry <achmadfikryy08@gmail.com>
2025-10-13 17:10:41 +0700

    Assertion body & header

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,5 +1,6 @@
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class Basics {
     public static void main(String[] args) {
@@ -27,6 +28,9 @@ public class Basics {
                         "  \"language\": \"French-IN\"\n" +
                         "}").
                 when().post("/maps/api/place/add/json").
-                then().log().all().assertThat().statusCode(200).extract().response().asString();
+                then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
     }
+
+    //Add Place -> Update Place with New Address -> Get Place to validate if New Address is present in response
+
 }
