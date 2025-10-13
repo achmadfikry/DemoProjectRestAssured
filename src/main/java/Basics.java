9984df088afec71cfbbb04a047d8f4e1b55ca12e
Achmad Fikry <achmadfikryy08@gmail.com>
2025-10-13 17:19:00 +0700

    move payload to another file

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,4 +1,6 @@
 import io.restassured.RestAssured;
+import org.example.files.payload;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -11,22 +13,7 @@ public class Basics {
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         given().log().all().queryParam("qaclick", "qaclick123").header("Content-Type", "application/json")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Frontline house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}").
+                .body(payload.AddPlace()).
                 when().post("/maps/api/place/add/json").
                 then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
     }
