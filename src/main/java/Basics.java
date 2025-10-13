6b9d337823c7d39d421e3ea0e067d1f91577f077
Achmad Fikry <achmadfikryy08@gmail.com>
2025-10-13 21:01:45 +0700

    convert log into string

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -12,10 +12,12 @@ public class Basics {
         //then - validate the response
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        given().log().all().queryParam("qaclick", "qaclick123").header("Content-Type", "application/json")
+        String response = given().log().all().queryParam("qaclick", "qaclick123").header("Content-Type", "application/json")
                 .body(payload.AddPlace()).
                 when().post("/maps/api/place/add/json").
                 then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
+
+        System.out.println(response);
     }
 
     //Add Place -> Update Place with New Address -> Get Place to validate if New Address is present in response
