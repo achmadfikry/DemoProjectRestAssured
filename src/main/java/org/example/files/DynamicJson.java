4179fffa177ab8dfef512c91de418186db369f21
Achmad Fikry <achmadfikryy08@gmail.com>
2025-10-22 20:59:02 +0700

    Addition multidymentional array for DataProvider

--- src/main/java/org/example/files/DynamicJson.java
+++ src/main/java/org/example/files/DynamicJson.java
@@ -2,6 +2,7 @@ package org.example.files;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,13 +11,18 @@ public class DynamicJson {
 
     @Test
     public void AddBook(){
-        RestAssured.baseURI="http://216.10.245.166/Library";
+        RestAssured.baseURI="http://216.10.245.166/";
         String response = given().log().all().header("Content-Type", "application/json")
                 .body(payload.AddLibrary("adfgh", "12345"))
-                .when().post("/Addbook.php")
+                .when().post("Library/Addbook.php")
                 .then().log().all().assertThat().statusCode(200).extract().response().asString();
         JsonPath jsonPath = ReUsableMethods.rawToJson(response);
         String id = jsonPath.get("ID");
         System.out.println(id);
+    }
+
+    @DataProvider(name = "BooksData")
+    public Object[][] getData(){
+        return new Object[][] {{"book1","1111"},{"book2","2222"},{"book3","3333"}};
     }
 }
