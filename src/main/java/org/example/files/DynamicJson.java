42ffcdcae6fb2ff3415b8c6397c15e19c26bf88e
Achmad Fikry <achmadfikryy08@gmail.com>
2025-10-22 23:04:55 +0700

    Handle static json payload

--- src/main/java/org/example/files/DynamicJson.java
+++ src/main/java/org/example/files/DynamicJson.java
@@ -5,7 +5,12 @@ import io.restassured.path.json.JsonPath;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 
 public class DynamicJson {
 
@@ -25,4 +30,19 @@ public class DynamicJson {
     public Object[][] getData(){
         return new Object[][] {{"book1","1111"},{"book2","2222"},{"book3","3333"}};
     }
+
+    @Test
+    public void AddLocationByJsonFile() throws IOException {
+        RestAssured.baseURI="https://rahulshettyacademy.com";
+        String response = given().log().all().queryParam("qaclick", "qaclick123").header("Content-Type", "application/json")
+                .body(new String(Files.readAllBytes(Paths.get("src/main/java/org/example/files/AddPlace.json"))))
+                .when().post("/maps/api/place/add/json")
+                .then().log().all().assertThat().statusCode(200).extract().response().asString();
+        System.out.println(response);
+    }
+
+    public static String GenerateStringFromResource(String path) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(path)));
+    }
+
 }
